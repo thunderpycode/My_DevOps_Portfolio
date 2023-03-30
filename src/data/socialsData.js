93cbabef6d1da8f2f2eedeb9d351de24baff6265
thunderpycode <114567270+scriptwithabhi@users.noreply.github.com>
2023-03-30 17:42:02 +0530

    fixed merge conflict

--- src/data/socialsData.js
+++ src/data/socialsData.js
@@ -1,27 +1,14 @@
 export const socialsData = {
-<<<<<<< HEAD
     github: 'https://github.com/thunderpycode',
     facebook: 'https://www.facebook.com/',
     linkedIn: 'https://www.linkedin.com/in/abhishek-srivastava-565ab0132/',
     instagram: 'https://www.instagram.com/',
-=======
-    github: 'https://github.com/thunderpycode/',
-  /*  facebook: 'https://www.facebook.com/profile.php?id=100047654323640',*/
-    linkedIn: 'https://linkedin.com/in/abhishek-srivastava-565ab0132/',
-  /*  instagram: 'https://www.instagram.com/',
->>>>>>> 0b7c16a69c6d36b0ac22319b22aa43b7db77de6b
     codepen: 'https://codepen.io/',
     twitter: 'https://twitter.com/NorthAbhishek',
     reddit: 'https://www.reddit.com/user/',
-<<<<<<< HEAD
     blogger: 'https://www.blogger.com/',
     medium: 'https://medium.com/@abhisri784',
     stackOverflow: 'https://stackoverflow.com/users/',
-=======
-    blogger: 'https://www.blogger.com/', */
-    medium: 'https://medium.com/@abhisri784',
-   /* stackOverflow: 'https://stackoverflow.com/users/',
->>>>>>> 0b7c16a69c6d36b0ac22319b22aa43b7db77de6b
     gitlab: 'https://gitlab.com/',
-    youtube: 'https://youtube.com/' */
+    youtube: 'https://youtube.com/'
 }
