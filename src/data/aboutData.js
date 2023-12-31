b5f47604933e970bbc5a54621013254dc6797208
Abhishek Srivastava <abhisheksrivastava@Abhisheks-MacBook-Air.local>
2023-12-31 16:20:16 +0530

    edited aboutData

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "Who I am",
-    description1: "I am Abhishek Srivastava. I'm DevOps Engineer from Kanpur, Utter Pradesh.",
+    description1: "I am Abhishek Srivastava. A Techie and Individual Contributor hailed from Kanpur, Uttar Pradesh",
     description2: "A seasoned and results-driven DevOps Engineer with 3 years of hands-on experience in streamlining software development and delivery processes. Proficient in a wide range of DevOps tools and methodologies, Specialize in creating efficient CI/CD pipelines, automating workflows, and optimizing infrastructure for scalability. Excels in bridging the gap between development and operations, ensuring rapid, reliable, and secure software deployments. My passion for continuous improvement and problem-solving enables organizations to achieve operational excellence in today's dynamic IT landscape.",
     image: 1
 }
